e76975e31726cba50431627b227144b961d7da36
Jason Yang <jason-yang@users.noreply.github.com>
2023-06-12 20:24:18 -0700

    feat: test meta data for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Metadata } from "next";
+
+const title = "Jason Yang";
+const description = "Personal page for Jason Yang";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    type: "website",
+    title,
+    description,
+  },
+};
+
 export default function Home() {
   return <p>Content</p>;
 }
