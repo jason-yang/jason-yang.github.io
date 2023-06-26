e61bd76e04be3f47d18f57416877c1314e105862
Jason Yang <jason-yang@users.noreply.github.com>
2023-06-25 22:23:53 -0700

    feat: add home icon to home page breadcrumb

--- src/components/breadcrumbs/provider.tsx
+++ src/components/breadcrumbs/provider.tsx
@@ -7,6 +7,7 @@ import {
   createContext,
   useState,
 } from "react";
+import HomeIcon from "@heroicons/react/20/solid/HomeIcon";
 import { CrumbItem } from "./types";
 
 interface Context {
@@ -21,7 +22,15 @@ export const BreadcrumbsContext = createContext<Context>({
 
 export default function BreadcrumbsProvider({ children }: PropsWithChildren) {
   const [items, setItems] = useState<CrumbItem[]>([
-    { label: "Home", href: "/" },
+    {
+      label: (
+        <div className="flex flex-row items-center gap-1">
+          <HomeIcon className="h-4 w-4" />
+          Home
+        </div>
+      ),
+      href: "/",
+    },
   ]);
 
   return (
