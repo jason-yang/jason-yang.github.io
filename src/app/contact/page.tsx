33a8bd613687ad04786407d168b78be06bfd0248
Jason Yang <jason-yang@users.noreply.github.com>
2023-06-25 22:22:38 -0700

    fix: missing breadcrumb in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+import Breadcrumb from "@/components/breadcrumbs/breadcrumb";
 import Container from "@/components/container";
 import Header from "@/components/header";
 import { Metadata } from "next";
@@ -10,20 +11,23 @@ export const metadata: Metadata = {
 
 export default function ContactPage() {
   return (
-    <Container header={<Header>Contact details</Header>}>
-      <p>
-        You can contact me via LinkedIn:{" "}
-        <a href="https://www.linkedin.com/in/jasonxwyang/" target="_blank">
-          <Image
-            src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white"
-            alt="LinkedIn"
-            unoptimized={true}
-            width={111}
-            height={28}
-            className="inline-block"
-          />
-        </a>
-      </p>
-    </Container>
+    <>
+      <Breadcrumb label="Contact" href="/contact" />
+      <Container header={<Header>Contact details</Header>}>
+        <p>
+          You can contact me via LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/jasonxwyang/" target="_blank">
+            <Image
+              src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white"
+              alt="LinkedIn"
+              unoptimized={true}
+              width={111}
+              height={28}
+              className="inline-block"
+            />
+          </a>
+        </p>
+      </Container>
+    </>
   );
 }
